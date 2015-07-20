47ce1027f766c66af98346c951d1668ecc92f4c0
Santiago Vidal <svidal91@hotmail.com>
2015-07-20 19:50:42 -0300

    Empezando a incorporar quagga

---
+++ configs/alice/intf.sh
@@ -0,0 +1,5 @@
+ifconfig alice-eth1 10.10.3.2 netmask 255.255.255.0
+ifconfig alice-eth2 10.10.2.2 netmask 255.255.255.0
+ifconfig alice-eth3 10.10.5.2 netmask 255.255.255.0
+
+
--- topo.py
+++ topo.py
@@ -22,16 +22,21 @@ class HostController(Host):
     self.cmd('sh ryu_start.sh &')
 
 class HybridNode(Host):
+  zebra_exec = '/usr/lib/quagga/zebra'
+  ospfd_exec = '/usr/lib/quagga/ospfd'
+  quaggaPath = '/usr/lib/quagga/'
   ovs_initd = "/etc/init.d/openvswitchd"
   baseDIR = "/tmp"
   OF_V = "OpenFlow13"
   
-  def __init__(self, name, loopback, ips, *args, **kwargs ):
-    dirs = ['/var/run', '/var/run/openvswitch']
+  def __init__(self, name, loopback, ips, dpid, *args, **kwargs ):
+    dirs = ['/var/log/', '/var/log/quagga', '/var/run', '/var/run/quagga', '/var/run/openvswitch']
     Host.__init__(self, name, privateDirs=dirs, *args, **kwargs )   
     self.loopback = loopback
     self.path_ovs = "%s/%s/ovs" %(self.baseDIR, self.name)
     self.ips = ips
+    self.dpid = dpid
+    self.path_quagga = "%s/%s/quagga" %(self.baseDIR, self.name)
 		
   def start(self):
     info("%s " % self.name)
@@ -53,27 +58,37 @@ class HybridNode(Host):
     self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait set-fail-mode %s secure" %(self.path_ovs, self.name))
     self.cmd('ovs-vsctl --db=unix:%s/db.sock --no-wait set bridge %s datapath_type=netdev' %(self.path_ovs, self.name))
     self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait set controller %s connection-mode=out-of-band" %(self.path_ovs, self.name))
-    
-    dp = '0000000000000002'
-    if self.name == 'alice':
-      dp = '0000000000000003'
-    if self.name == 'oz':
-      dp = '0000000000000001'
-    if self.name == 'possion':
-      dp = '0000000000000004'
-    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait -- set Bridge %s other_config:datapath-id=%s" %(self.path_ovs, self.name, dp))
+   
+    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait -- set Bridge %s other_config:datapath-id=%s" %(self.path_ovs, self.name, self.dpid))
     
     # Agregar controlador
     self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait set-controller %s tcp:192.168.1.10:6633" %(self.path_ovs, self.name))
     
     # Agregar puertos
-    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth1" %(self.path_ovs, self.name, self.name))
-    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth2" %(self.path_ovs, self.name, self.name))
-    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth3" %(self.path_ovs, self.name, self.name))
     self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth4" %(self.path_ovs, self.name, self.name))
     self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth5" %(self.path_ovs, self.name, self.name))
+    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth6" %(self.path_ovs, self.name, self.name))
+    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth7" %(self.path_ovs, self.name, self.name))
+    self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s-eth8" %(self.path_ovs, self.name, self.name))
     #self.cmd("ovs-vsctl --db=unix:%s/db.sock --no-wait add-port %s %s -- set Interface %s type=internal" %(self.path_ovs, self.name, 
     #vi_name, vi_name))
+    
+    # Configuracion de Quagga
+    os.mkdir(self.path_quagga)
+    zebra_conf = open(self.path_quagga + "/zebra.conf","w")
+    ospfd_conf = open(self.path_quagga + "/ospfd.conf","w")
+    
+    ospfd_conf.write("hostname %s\n" % self.name)
+    ospfd_conf.write("password zebra\n")
+    ospfd_conf.write("log file /var/log/quagga/ospfd.log\n\n")
+    
+    zebra_conf.write("hostname %s\n" % self.name)
+    zebra_conf.write("password zebra\n")
+    zebra_conf.write("enable password zebra\n")
+    zebra_conf.write("log file /var/log/quagga/zebra.log\n\n")
+    
+    ospfd_conf.close()
+    zebra_conf.close()
       
       
     # Otras configs
@@ -83,8 +98,8 @@ class HybridNode(Host):
       i = i + 1
       self.cmd('ifconfig %s-eth%s %s netmask 255.255.255.0' %(self.name, str(i), ip))
     self.cmd('sysctl -w net.ipv4.ip_forward=0')
-    self.cmd('ifconfig %s-eth1 10.0.0.7 netmask 255.255.0.0' % self.name)
-    self.cmd('ifconfig %s-eth2 10.1.0.7 netmask 255.255.0.0' % self.name)
+    self.cmd('ifconfig %s-eth7 10.0.0.7 netmask 255.255.0.0' % self.name)
+    self.cmd('ifconfig %s-eth8 10.1.0.7 netmask 255.255.0.0' % self.name)
     
 
 
@@ -99,22 +114,22 @@ class MyTopo( Topo ):
     
     # Galois
     galois = self.addHost('galois', loopback="127.0.0.1",
-			  ips=['10.10.1.1','10.10.5.1','10.10.4.1','192.168.1.11'],
-			  cls=HybridNode)
+			  ips=['192.168.1.11','10.10.1.1','10.10.5.1','10.10.4.1'],
+			  dpid='0000000000000001', cls=HybridNode)
     # Oz
     oz = self.addHost('oz', loopback="127.0.0.1",
-		      ips=['10.10.1.2','10.10.6.2','10.10.3.1','192.168.1.12'],
-		      cls=HybridNode)
+		      ips=['192.168.1.12','10.10.1.2','10.10.6.2','10.10.3.1'],
+		      dpid='0000000000000002', cls=HybridNode)
     
     # Possion
     possion = self.addHost('possion', loopback="127.0.0.1",
-			  ips=['10.10.2.1','10.10.6.1','10.10.4.2','192.168.1.13'],
-			  cls=HybridNode)
+			  ips=['192.168.1.13','10.10.2.1','10.10.6.1','10.10.4.2'],
+			  dpid='0000000000000003', cls=HybridNode)
     
     # Alice
     alice = self.addHost('alice', loopback="127.0.0.1",
-			  ips=['10.10.2.2','10.10.5.2','10.10.3.2','192.168.1.14'],
-			  cls=HybridNode)
+			  ips=['192.168.1.14','10.10.2.2','10.10.5.2','10.10.3.2'],
+			  dpid='0000000000000004', cls=HybridNode)
     
     # Controlador
     root = self.addHost('controller', cls=HostController,
@@ -124,21 +139,21 @@ class MyTopo( Topo ):
     man_switch = self.addSwitch('s1', protocols='OpenFlow13', failMode='standalone')
 
     # Enlaces
-    self.addLink(man_switch, galois, 1, 6)
-    self.addLink(man_switch, oz, 2, 6)
-    self.addLink(man_switch, possion, 3, 6)
-    self.addLink(man_switch, alice, 4, 6)
+    self.addLink(man_switch, galois, 1, 3)
+    self.addLink(man_switch, oz, 2, 3)
+    self.addLink(man_switch, possion, 3, 3)
+    self.addLink(man_switch, alice, 4, 3)
     self.addLink(man_switch, root, 0, 0)
     
-    self.addLink(galois, alice, 4, 4)
-    self.addLink(galois, possion, 5, 5)
-    self.addLink(galois, oz, 3, 3)
-    self.addLink(alice, possion, 3, 3)
-    self.addLink(alice, oz, 5, 5)
-    self.addLink(oz, possion, 4, 4)
+    self.addLink(galois, alice, 5, 5)
+    self.addLink(galois, possion, 6, 6)
+    self.addLink(galois, oz, 4, 4)
+    self.addLink(alice, possion, 4, 4)
+    self.addLink(alice, oz, 6, 6)
+    self.addLink(oz, possion, 5, 5)
     
-    self.addLink(alice, h0, 1, 0)
-    self.addLink(alice, h1, 2, 0)
+    self.addLink(alice, h0, 7, 0)
+    self.addLink(alice, h1, 8, 0)
 
 
 
