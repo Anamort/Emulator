8c1e811c0a6396dcb4d6be9afff5768aeef813ab
Santiago Vidal <svidal91@hotmail.com>
2015-08-30 21:33:44 -0300

    empezando extension ovs para las interfaces

--- utils/wsOVS.py
+++ utils/wsOVS.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 from flask import Flask
 import commands
+import re
 
 
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/ovs/atp/<address>')
+#@app.route('/snmp/atp/<address>')
 def getInfo(address):
     print address
-    tempArray = commands.getstatusoutput('snmpget -v2c -c public '+ address +' .1.3.6.1.4.1.8888')
-    temp =tempArray[1]
-    resultArray = temp.split('STRING:')
-    result = resultArray[1].strip()
-    return result
+    tempArray = commands.getstatusoutput('ovs-vsctl --db=tcp:%s:6640 list bridge' % address)
+    regexp = re.compile('.*ip_addresses="(.*)"}', re.DOTALL)
+    field = regexp.match(tempArray[1]).group(1)
+    interfaces = field.split("/")
+    print interfaces
+    #temp =tempArray[1]
+    #resultArray = temp.split('STRING:')
+    #result = resultArray[1].strip()
+    #return result
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  getInfo("192.168.1.14")
+    #app.run(host='0.0.0.0')
 	#default port: 5000
+	
