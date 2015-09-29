4cc829d02b6710430948e7e195acc56f8937290e
Santiago Vidal <svidal91@hotmail.com>
2015-09-29 20:10:34 -0300

    arreglo en script del controlador y archivo para debuggear el problema con quagga

--- utils/ryu_start.sh
+++ utils/ryu_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /home/santiago/LiveCode-master/ryu-master
-PYTHONPATH=. ./bin/ryu run --observe-links ryu/app/proyecto/businessLogic/rauflowapp.py
+# ./bin/ryu run --observe-links ryu/app/gui_topology/gui_topology.py
+./bin/ryu run --observe-links ryu/app/proyecto/businessLogic/gui_topology.py
