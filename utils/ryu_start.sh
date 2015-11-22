4a529b50d8c098d4c8dd218ece940aa41f71c8c2
Santiago Vidal <svidal91@hotmail.com>
2015-11-22 15:19:21 -0200

    modificando el ryu start

--- utils/ryu_start.sh
+++ utils/ryu_start.sh
@@ -2,4 +2,4 @@
 
 cd /home/santiago/P2015_44-Controlador/LiveCode/ryu-master
 # ./bin/ryu run --observe-links ryu/app/gui_topology/gui_topology.py
-PYTHONPATH=. ./bin/ryu run --observe-links ryu/app/proyecto/businessLogic/gui_topology.py
+PYTHONPATH=. ./bin/ryu run --observe-links ryu/app/proyecto/businessLogic/gui_topology.py >> /home/santiago/P2015_44/salida.txt 2>&1
