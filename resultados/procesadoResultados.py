18c1296f42a62fb85f5c63eb06c66396da1cc16f
Santiago Vidal <svidal91@hotmail.com>
2016-08-12 20:54:15 -0300

    procesando resultados de topologia mediana

--- resultados/procesadoResultados.py
+++ resultados/procesadoResultados.py
@@ -1,29 +1,39 @@
-f = open("resultados/tiemposMedium4.txt","r")
+f = open("resultados/tiemposMedium.txt","r")
 
 lines = f.readlines()
 
-porcentajesCamino = []
-porcentajesMPLS = []
-porcentajesFlujos = []
+porcentajesCaminoCapa2 = []
+porcentajesCaminoCapa3 = []
+porcentajesMPLSCapa2 = []
+porcentajesMPLSCapa3 = []
+porcentajesFlujosCapa2 = []
+porcentajesFlujosCapa3 = []
 
 for line in lines:
-	if "    --> Camino: " in line:
-		porcentajesCamino.append(float(line[16:-2]))
+	if "Capa2    --> Camino: " in line:
+		porcentajesCaminoCapa2.append(float(line[21:-2]))
 
-	if "    --> MPLS: " in line:
-		porcentajesMPLS.append(float(line[14:-2]))
+	if "Capa2    --> MPLS: " in line:
+		porcentajesMPLSCapa2.append(float(line[19:-2]))
 
-	if "    --> Flujos " in line:
-		porcentajesFlujos.append(float(line[15:-2]))
+	if "Capa2    --> Flujos " in line:
+		porcentajesFlujosCapa2.append(float(line[20:-2]))
 
-	if (len(porcentajesFlujos) == 8) and (len(porcentajesCamino) == 8) and (len(porcentajesMPLS) == 8):
-		print "VPN Capa 2 -- Promedio Camino: " + str(sum(porcentajesCamino)/len(porcentajesCamino))
-		print "VPN Capa 2 -- Promedio MPLS: " + str(sum(porcentajesMPLS)/len(porcentajesMPLS))
-		print "VPN Capa 2 -- Promedio Flujos: " + str(sum(porcentajesFlujos)/len(porcentajesFlujos))
-		porcentajesCamino = []
-		porcentajesFlujos = []
-		porcentajesMPLS = []
+	if "Capa3    --> Camino: " in line:
+		porcentajesCaminoCapa3.append(float(line[21:-2]))
 
-print "VPN Capa 3 -- Promedio Camino: " + str(sum(porcentajesCamino)/len(porcentajesCamino))
-print "VPN Capa 3 -- Promedio MPLS: " + str(sum(porcentajesMPLS)/len(porcentajesMPLS))
-print "VPN Capa 3 -- Promedio Flujos: " + str(sum(porcentajesFlujos)/len(porcentajesFlujos))
+	if "Capa3    --> MPLS: " in line:
+		porcentajesMPLSCapa3.append(float(line[19:-2]))
+
+	if "Capa3    --> Flujos " in line:
+		porcentajesFlujosCapa3.append(float(line[20:-2]))
+
+
+
+print "VPN Capa 2 -- Promedio Camino: " + str(sum(porcentajesCaminoCapa2)/len(porcentajesCaminoCapa2))
+print "VPN Capa 2 -- Promedio MPLS: " + str(sum(porcentajesMPLSCapa2)/len(porcentajesMPLSCapa2))
+print "VPN Capa 2 -- Promedio Flujos: " + str(sum(porcentajesFlujosCapa2)/len(porcentajesFlujosCapa2))
+
+print "VPN Capa 3 -- Promedio Camino: " + str(sum(porcentajesCaminoCapa3)/len(porcentajesCaminoCapa3))
+print "VPN Capa 3 -- Promedio MPLS: " + str(sum(porcentajesMPLSCapa3)/len(porcentajesMPLSCapa3))
+print "VPN Capa 3 -- Promedio Flujos: " + str(sum(porcentajesFlujosCapa3)/len(porcentajesFlujosCapa3))
